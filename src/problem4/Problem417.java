6b208bc28e9db4219f2b8dec27597936742c6bc9
井上雄太 <s1341011we@gmail.com>
2023-07-07 03:15:58 +0900

    [Problem418]

--- src/problem4/Problem417.java
+++ src/problem4/Problem417.java
@@ -1,5 +1,17 @@
 package problem4;
 
 public class Problem417 {
-
+	
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if (j <= i) {
+                    System.out.print("■");
+                } else {
+                    System.out.print("□");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
